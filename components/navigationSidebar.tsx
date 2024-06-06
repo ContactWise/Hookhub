74f13d01d70c4c4fb6686bbdf83bef5853c89603
ary808 <aryaman@heliverse.com>
2024-06-06 10:32:53 +0530

    init

---
+++ components/navigationSidebar.tsx
@@ -0,0 +1,40 @@
+import { Home, LineChart, Package, ShoppingCart, Users } from "lucide-react";
+import Link from "next/link";
+import { Badge } from "./ui/badge";
+import { FC } from "react";
+
+interface NavigationSidebarProps {
+  links: {
+    href: string;
+    text: string;
+    icon: JSX.Element;
+  }[];
+}
+
+const NavigationSidebar: FC<NavigationSidebarProps> = ({ links }) => {
+  return (
+    <div className="hidden border-r bg-muted/40 md:block">
+      <div className="flex h-full max-h-screen flex-col gap-2">
+        <div className="flex h-14 items-center border-b px-4 lg:h-[100px] lg:px-6">
+          <h1 className="text-primary text-4xl font-bold">Hookhub.</h1>
+        </div>
+        <div className="flex-1">
+          <nav className="grid items-start gap-2 px-2 text-sm font-medium lg:px-4">
+            {links.map((link) => (
+              <Link
+                href={link.href}
+                key={link.text}
+                className="flex items-center gap-3 rounded-lg px-3 py-3 text-muted-foreground transition-all hover:text-primary hover:bg-secondary"
+              >
+                {link.icon}
+                {link.text}
+              </Link>
+            ))}
+          </nav>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavigationSidebar;
